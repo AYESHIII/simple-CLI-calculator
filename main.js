046b230b92222c69296decad89a8ff03628b5aa0
AYESHIII <saadamjad562@gmail.com>
2024-07-16 12:57:01 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blueBright.bold("\n\t WELCOME TO  SHEIKH'S CALCULATOR \n\t"));
+// asking question from users through inquirer
+let answers = await inquirer.prompt([
+    { message: "enter firstNumber", type: "number", name: "firstNumber" },
+    { message: "enter secondNumber", type: "number", name: "secondNumber" },
+    {
+        message: "select one operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// conditional statement IF ELSE
+if (answers.operator === "Addition") {
+    console.log(chalk.green.bold(answers.firstNumber + answers.secondNumber));
+}
+else if (answers.operator === "Subtraction") {
+    console.log(chalk.green.bold(answers.firstNumber - answers.secondNumber));
+}
+else if (answers.operator === "Multiplication") {
+    console.log(chalk.green.bold(answers.firstNumber * answers.secondNumber));
+}
+else if (answers.operator === "Division") {
+    console.log(chalk.green.bold(answers.firstNumber / answers.secondNumber));
+}
+else {
+    console.log(chalk.redBright.bold("invalid output"));
+}
